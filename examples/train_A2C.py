bb9a259fb0ba6b14550133e71c09f3c2a1b3d795
Javier Arroyo <javier.arroyo@kuleuven.be>
2021-01-25 10:18:25 +0100

    Revert "Finished training a2c_C_1e6"
    This reverts commit 3dfeeb01ac514bcc0821500743233482717df3aa.

--- examples/train_A2C.py
+++ examples/train_A2C.py
@@ -4,10 +4,8 @@ case. This case needs to be deployed to run this script.
 
 '''
 
-from boptestGymEnv import BoptestGymEnvRewardWeightCost, NormalizedActionWrapper, \
-    NormalizedObservationWrapper, SaveOnBestTrainingRewardCallback
+from boptestGymEnv import BoptestGymEnvRewardWeightCost, NormalizedActionWrapper, NormalizedObservationWrapper
 from stable_baselines import A2C
-from stable_baselines.bench import Monitor
 from examples.test_and_plot import test_agent
 from collections import OrderedDict
 from testing import utilities
@@ -23,6 +21,7 @@ random.seed(seed)
 def train_A2C(start_time_tests    = [31*24*3600, 304*24*3600], 
               episode_length_test = 14*24*3600, 
               load                = False,
+              tensorboard_log     = os.path.join('results'),
               case                = 'simple'):
     '''Method to train (or load a pre-trained) A2C agent. Testing periods 
     have to be introduced already here to not use these during training. 
@@ -40,6 +39,8 @@ def train_A2C(start_time_tests    = [31*24*3600, 304*24*3600],
     load : boolean
         Boolean indicating whether the algorithm is loaded (True) or 
         needs to be trained (False)
+    tensorboard_log : path
+        Path to directory to load tensorboard logs.
     case : string
         Case to be tested.
         
@@ -107,26 +108,18 @@ def train_A2C(start_time_tests    = [31*24*3600, 304*24*3600],
     env = NormalizedObservationWrapper(env)
     env = NormalizedActionWrapper(env)  
     
-    # Create a log directory
-    log_dir = os.path.join(utilities.get_root_path(), 'examples', 'agents', 'A2C_{}_logdir'.format(case))
-    os.makedirs(log_dir, exist_ok=True)
-    
-    # Modify the environment to include the callback
-    env = Monitor(env=env, filename=os.path.join(log_dir,'monitor.csv'))
-    
-    # Create the callback: check every 1000 steps 
-    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
-    
     model = A2C('MlpPolicy', env, verbose=1, gamma=0.99, seed=seed,
-                tensorboard_log=log_dir, n_cpu_tf_sess=1)
+                tensorboard_log=tensorboard_log)
     
     if not load: 
-        model.learn(total_timesteps=int(1e6), callback=callback)
+        model.learn(total_timesteps=int(1e5))
         # Save the agent
-        model = A2C.save(save_path=os.path.join(log_dir,'last_model'))
+        model = A2C.save(os.path.join(utilities.get_root_path(), 'examples',
+                                      'agents', 'a2c_{}'.format(case)))
     else:
         # Load the trained agent
-        model = A2C.load(load_path=os.path.join(log_dir,'last_model'))
+        model = A2C.load(os.path.join(utilities.get_root_path(), 'examples',
+                                      'agents', 'a2c_{}'.format(case)))
     
     return env, model, start_time_tests
         
@@ -157,9 +150,9 @@ def test_nov(env, model, start_time_tests,
     return observations, actions, rewards, kpis
 
 if __name__ == "__main__":
-    env, model, start_time_tests = train_A2C(load=True, case='C')
+    env, model, start_time_tests = train_A2C(load=True)
     episode_length_test = 14*24*3600
-    warmup_period_test  = 1*24*3600
+    warmup_period_test  = 3*24*3600
     plot = True
     test_feb(env, model, start_time_tests, episode_length_test, warmup_period_test, plot)
     test_nov(env, model, start_time_tests, episode_length_test, warmup_period_test, plot)
