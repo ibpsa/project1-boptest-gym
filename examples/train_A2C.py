68ddf0dd0947a950f7b0ccb9c4f39724632bbca5
Javier Arroyo <javier.arroyo@kuleuven.be>
2021-04-16 13:00:37 +0200

    Change reward function

--- examples/train_A2C.py
+++ examples/train_A2C.py
@@ -82,7 +82,7 @@ def train_A2C(start_time_tests    = [(45-7)*24*3600, (310-7)*24*3600],
             kpis = requests.get('{0}/kpi'.format(self.url)).json()
             
             # Calculate objective integrand function at this point
-            objective_integrand = kpis['cost_tot']/3. + 0.1*kpis['tdis_tot']
+            objective_integrand = kpis['cost_tot'] + 10*kpis['tdis_tot']
             
             # Compute reward
             reward = -(objective_integrand - self.objective_integrand)
