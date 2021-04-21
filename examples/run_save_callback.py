db3f7d0adcf67fb73c4abcc49032bd2f8550ffb1
Javier Arroyo <javier.arroyo@kuleuven.be>
2021-04-21 13:57:02 +0200

    Updates needed for SaveAndTestCallback.

--- examples/run_save_callback.py
+++ examples/run_save_callback.py
@@ -6,7 +6,7 @@ performance and saves a model upon improved performance.
 
 '''
 
-from boptestGymEnv import BoptestGymEnvRewardWeightCost, NormalizedActionWrapper, NormalizedObservationWrapper, SaveOnBestTrainingRewardCallback
+from boptestGymEnv import BoptestGymEnvRewardWeightCost, NormalizedActionWrapper, NormalizedObservationWrapper, SaveAndTestCallback
 from stable_baselines import A2C
 from stable_baselines.bench import Monitor
 from testing import utilities
@@ -70,7 +70,7 @@ def train_A2C_with_callback(start_time_tests    = [31*24*3600, 304*24*3600],
     env = Monitor(env=env, filename=os.path.join(log_dir,'monitor.csv'))
     
     # Create the callback: check every 10 steps. We keep it very short for testing 
-    callback = SaveOnBestTrainingRewardCallback(check_freq=10, log_dir=log_dir)
+    callback = SaveAndTestCallback(env=env, check_freq=10, log_dir=log_dir)
     
     # Initialize the agent
     model = A2C('MlpPolicy', env, verbose=1, gamma=0.99, seed=seed,
