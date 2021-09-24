0fd8c718e0c1d4605b2682f79a6eb280741f741a
Javier Arroyo <javier.arroyo@kuleuven.be>
2021-09-24 12:35:55 +0200

    Improve test_many.py

--- examples/test_many.py
+++ examples/test_many.py
@@ -4,32 +4,34 @@ Test many RL cases to generate KPIs
 '''
 
 from examples.train_RL import train_RL, test_peak, test_typi
-
 import matplotlib.pyplot as plt
-from scipy import interpolate
-from gym.core import Wrapper
-import matplotlib.dates as mdates
-import numpy as np
-import pandas as pd
-import requests
-import json
-import os
-
-
 
 if __name__ == "__main__":
-    render = True
-    plot = not render # Plot does not work together with render
-    
-    env, model, start_time_tests, log_dir = train_RL(algorithm='DQN', mode='train', case='D', training_timesteps=1e6, render=render, expert_traj=os.path.join('trajectories','expert_traj_disc_28.npz'))
-    
+    render = False
+    plot = not render # Plot does not work together with render    
     warmup_period_test  = 7*24*3600
     episode_length_test = 14*24*3600
-    kpis_to_file = True
+    save_to_file = True
 
-    test_peak(env, model, start_time_tests, episode_length_test, warmup_period_test, log_dir, kpis_to_file, plot)
-    test_typi(env, model, start_time_tests, episode_length_test, warmup_period_test, log_dir, kpis_to_file, plot)
-    
+    model_names = [
+        'model_10000',
+        'model_50000',
+        'model_100000',
+        'model_200000',
+        'model_300000',
+        'model_400000',
+        'model_500000',
+        'model_600000',
+        'model_700000',
+        'model_800000',
+        'model_900000',
+        'model_1000000',
+        ]
 
+    for model_name in model_names:
+        env, model, start_time_tests, log_dir = train_RL(algorithm='DQN', mode='load', case='C', training_timesteps=1e6, render=render, model_name=model_name)
+        plt.close('all')
+        test_peak(env, model, start_time_tests, episode_length_test, warmup_period_test, log_dir, model_name, save_to_file, plot)
+        test_typi(env, model, start_time_tests, episode_length_test, warmup_period_test, log_dir, model_name, save_to_file, plot)
     
     
