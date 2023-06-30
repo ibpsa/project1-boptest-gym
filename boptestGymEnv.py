ff1a1275bd5985da3ccf5f4298edda83688e9839
Javier Arroyo <javier.arroyo@kuleuven.be>
2023-06-30 11:22:16 +0200

    Remove forecast parameters from summary.

--- boptestGymEnv.py
+++ boptestGymEnv.py
@@ -170,8 +170,6 @@ class BoptestGymEnv(gym.Env):
         self.all_input_vars = requests.get('{0}/inputs'.format(url)).json()['payload']
         # Default simulation step
         self.step_def = requests.get('{0}/step'.format(url)).json()['payload']
-        # Default forecast parameters
-        self.forecast_def = requests.get('{0}/forecast_parameters'.format(url)).json()['payload']
         # Default scenario
         self.scenario_def = requests.get('{0}/scenario'.format(url)).json()['payload']
         
@@ -359,7 +357,6 @@ class BoptestGymEnv(gym.Env):
         summary['BOPTEST CASE INFORMATION']['All forecasting variables'] = pformat(list(self.all_predictive_vars.keys()))
         summary['BOPTEST CASE INFORMATION']['All input variables'] = pformat(self.all_input_vars)
         summary['BOPTEST CASE INFORMATION']['Default simulation step (seconds)'] = pformat(self.step_def)
-        summary['BOPTEST CASE INFORMATION']['Default forecasting parameters (seconds)'] = pformat(self.forecast_def)
         summary['BOPTEST CASE INFORMATION']['Default scenario'] = pformat(self.scenario_def)
         summary['BOPTEST CASE INFORMATION']['Test case scenario'] = pformat(self.scenario)
         
